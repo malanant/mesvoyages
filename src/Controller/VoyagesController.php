0d4319183ac6d89839de321207476dc88d1695a9
mlavenant <mlavenant@DESKTOP-KFQGHAR.localdomain>
2023-11-22 12:59:58 +0100

    Phase 3, fin de cours

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -12,15 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class AccueilController extends AbstractController{
+class VoyagesController extends AbstractController{
     
    /**
     * 
-    *@Route("/", name="accueil")
+    *@Route("/voyages", name="voyages")
     *@return Response
     */
     
    public function index(): Response{
-       return $this->render("pages/accueil.html.twig");
+       return $this->render("pages/voyages.html.twig");
    }
 }
